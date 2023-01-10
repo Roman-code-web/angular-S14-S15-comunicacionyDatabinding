9dea70b880462096f441304bbd7172a0951ba2b2
Roman-code-web <selenerg.275@gmail.com>
2023-01-10 12:12:31 -0500

    estilos home e hijo actualizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataBindingComponent } from './pages/data-binding/data-binding.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+  {path:"dataBinding", component:DataBindingComponent},
+  { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
 @NgModule({
